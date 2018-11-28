0f0550f12d3002d1fbbfefebc10d63cb4982c127
Артём Грибков <artemgribkov@iMac-Artem.local>
2018-11-29 02:08:02 +0300

    Initial

---
+++ main.js
@@ -0,0 +1,25 @@
+const fileInput = window.document.getElementById('fileInput');
+const imageBox = window.document.getElementById('imageBox');
+
+fileInput.addEventListener('change', event => {
+    const width = 500;
+    const height = 300;
+    const reader = new FileReader();
+
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = event => {
+        const img = new Image();
+        img.src = event.target.result;
+
+        img.onload = () => {
+            const elem = document.createElement('canvas');
+            elem.width = width;
+            elem.height = height;
+            const ctx = elem.getContext('2d');
+            ctx.drawImage(img, 0, 0, width, height);
+            imageBox.innerHTML = `<img src="${elem.toDataURL('image/jpeg')}" />`;
+        };
+
+        reader.onerror = error => console.log(error);
+    };
+});
